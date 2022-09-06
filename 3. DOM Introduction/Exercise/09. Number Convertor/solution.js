f603e8e1d78f1e42551ab6e787bb3b2d882190fb
flnx <32278279+flnx@users.noreply.github.com>
2022-09-06 12:32:59 +0300

    DOM Exercises

--- 3. DOM Introduction/Exercise/09. Number Convertor/solution.js
+++ 3. DOM Introduction/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,27 @@
 function solve() {
+  const selectElements = document.getElementById('selectMenuTo');
 
-    //TODO...
+  const binaryOption = document.createElement('option');
+  binaryOption.value = 'binary';
+  binaryOption.textContent = 'Binary';
+  selectElements.appendChild(binaryOption);
+
+  const hexadecimalOption = document.createElement('option');
+  hexadecimalOption.value = 'hexadecimal';
+  hexadecimalOption.textContent = 'Hexadecimal';
+  selectElements.appendChild(hexadecimalOption);
+  
+  const convertBtn = document.querySelector('button');
+  
+  convertBtn.addEventListener('click', () => {
+    const result = document.getElementById('result');
+    const userNum = Number(document.getElementById('input').value);
+    
+    console.log(selectElements.value);
+    if (selectElements.value === 'binary') {
+      result.value = Number(userNum).toString(2);
+    } else if (selectElements.value === 'hexadecimal') {
+      result.value = Number(userNum).toString(16).toUpperCase();
+    }
+  });
 }
