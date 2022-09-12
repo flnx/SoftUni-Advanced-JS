495bb674b6f1bd6d70fb38b65bf73d8c43bef0fa
flnx <32278279+flnx@users.noreply.github.com>
2022-09-12 15:30:53 +0300

    Exercises

--- 4. DOM Manipulations and Events/Exercise/07. Distance-Converter/distanceConverter.js
+++ 4. DOM Manipulations and Events/Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,50 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+  const selectFromElem = document.getElementById('inputUnits');
+  const selectToElem = document.getElementById('outputUnits');
+
+  const convertBtn = document.getElementById('convert');
+
+  convertBtn.addEventListener('click', () => {
+    let input = Number(document.getElementById('inputDistance').value);
+    let result = 0;
+
+    switch (selectFromElem.value) {
+      case 'km':
+        input *= 1000; break;
+      case 'm':
+        input = input; break;
+      case 'cm':
+        input *= 0.01; break;
+      case 'mm':
+        input *= 0.001; break;
+      case 'mi':
+        input *= 1609.34; break;
+      case 'yrd':
+        input *= 0.9144; break;
+      case 'ft':
+        input *= 0.3048; break;
+      case 'in':
+        input *= 0.0254; break;
+    }
+
+    switch (selectToElem.value) {
+      case 'km':
+        result = input / 1000; break;
+      case 'm':
+        result = input; break;
+      case 'cm':
+        result = input / 0.01; break;
+      case 'mm':
+        result = input / 0.001; break;
+      case 'mi':
+        result = input / 1609.34; break;
+      case 'yrd':
+        result = input / 0.9144; break;
+      case 'ft':
+        result = input / 0.3048; break;
+      case 'in':
+        result = input / 0.0254; break;
+      }
+      document.getElementById('outputDistance').value = result;
+  });
 }
--- 4. DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
+++ 4. DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,47 @@
 function solve() {
-  //TODO...
+  const answers = document.querySelectorAll('.answer-wrap');
+
+  for (let answer of answers) {
+    answer.addEventListener('click', onClick);
+  }
+
+  const correctAnswers = [
+    'onclick',
+    'JSON.stringify()',
+    'A programming API for HTML and XML documents',
+  ];
+
+  let counter = 0;
+  let index = 1;
+
+  function onClick(e) {
+    const sections = document.querySelectorAll('section');
+    const userAnswer = e.target.textContent;
+
+    
+    if (userAnswer == correctAnswers[0]) {
+      console.log(userAnswer);
+      counter++;
+    }
+    correctAnswers.shift();
+
+    // const parentElement = e.target.closest('section'); // doesn't work in SoftUni Judge System 
+    const parentElement = e.target.parentElement.parentElement.parentElement.parentElement;
+    console.log(parentElement);
+
+    parentElement.style.display = 'none';
+
+    if (sections[index]) {
+      sections[index].style.display = 'block';
+      index++;
+    } else {
+      const output = document.querySelector('#results h1');
+      if (counter == 3) {
+        output.textContent = 'You are recognized as top JavaScript fan!';
+      } else {
+        output.textContent = `You have ${counter} right answers`;
+      }
+      document.getElementById('results').style.display = 'block';
+    }
+  }
 }
