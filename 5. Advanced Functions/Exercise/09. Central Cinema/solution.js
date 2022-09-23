30d99c033bef6642dd0229ae25cf3063acf278c2
flnx <32278279+flnx@users.noreply.github.com>
2022-09-24 00:15:54 +0300

    Adding and updating

--- 5. Advanced Functions/Exercise/09. Central Cinema/solution.js
+++ 5. Advanced Functions/Exercise/09. Central Cinema/solution.js
@@ -1,14 +1,11 @@
 function solve() {
   const [movieName, hallName, priceElem] = document.querySelectorAll('#container input');
   const [screenBtn, clearBtn] = document.querySelectorAll('button');
+  screenBtn.addEventListener('click', addMovie);
 
-  screenBtn.addEventListener('click', onScreen);
+  clearBtn.addEventListener('click', () => Array.from(document.querySelectorAll('#archive li')).map((x) => x.remove()));
 
-  clearBtn.addEventListener('click', () =>
-    Array.from(document.querySelectorAll('#archive li')).map((x) => x.remove())
-  );
-
-  function onScreen(e) {
+  function addMovie(e) {
     e.preventDefault();
     const fields = Array.from(document.querySelectorAll('#container input'));
     const isFieldValid = fields.every((x) => x.value);
@@ -21,48 +18,45 @@ function solve() {
 
   function elementsCreator() {
     const li = document.createElement('li');
-    li.appendChild(create('span', movieName.value));
-    li.appendChild(create('strong', `Hall: ${hallName.value}`));
-
-    const div = create('div');
-    div.appendChild(create('strong', Number(priceElem.value).toFixed(2)));
-
-    const input = create('input');
-    input.placeholder = 'Tickets Sold';
-    div.appendChild(input);
-
+    const hall = create('strong', `Hall: ${hallName.value}`);
+    const div = document.createElement('div');
+    const input = document.createElement('input');
     const archiveBtn = create('button', 'Archive');
-    archiveBtn.addEventListener('click', archive);
-    div.appendChild(archiveBtn);
 
+    archiveBtn.addEventListener('click', archive);
+    input.placeholder = 'Tickets Sold';
+
+    li.appendChild(create('span', movieName.value));
+    li.appendChild(hall);
+    div.appendChild(create('strong', Number(priceElem.value).toFixed(2)));
+    div.appendChild(input);
+    div.appendChild(archiveBtn);
     li.appendChild(div);
     document.querySelector('#movies ul').appendChild(li);
-  }
 
-  function archive(e) {
-    const qty = Number(e.target.previousElementSibling.value);
-    const inputField = e.target.previousElementSibling.value;
+    function archive(e) {
+      const qty = Number(input.value);
+      const inputField = input.value;
 
-    if (!isNaN(qty) && inputField !== '') {
-      const price = Number(e.target.parentElement.children[0].textContent);
-      const totalPrice = create('strong', `Total amount: ${(qty * price).toFixed(2)}`);
-      const deleteBtn = create('button', 'Delete');
-      deleteBtn.addEventListener('click', (e) => e.target.parentElement.remove());
-      const parentElement = e.target.parentElement.parentElement;
+      if (!isNaN(qty) && inputField !== '') {
+        const price = Number(e.target.parentElement.children[0].textContent);
+        const totalPrice = create('strong', `Total amount: ${(qty * price).toFixed(2)}`);
+        const deleteBtn = create('button', 'Delete');
+        deleteBtn.addEventListener('click', () => li.remove());
 
-      parentElement.children[1].remove();
-      e.target.parentElement.remove();
-      parentElement.appendChild(totalPrice);
-      parentElement.appendChild(deleteBtn);
-      document.querySelector('#archive ul').appendChild(parentElement);
+        div.remove();
+        hall.remove();
+        li.appendChild(totalPrice);
+        li.appendChild(deleteBtn);
+
+        document.querySelector('#archive ul').appendChild(li);
+      }
     }
   }
 
   function create(type, text) {
     const item = document.createElement(type);
-    if (text) {
-      item.appendChild(document.createTextNode(text));
-    }
+    item.appendChild(document.createTextNode(text));
     return item;
   }
 }
--- 5. Advanced Functions/Exercise/10. Task Manager/app.js
+++ 5. Advanced Functions/Exercise/10. Task Manager/app.js
@@ -1,3 +1,50 @@
 function solve() {
-    console.log("//TODO")
+  const [task, description, date] = document.querySelectorAll('form input, textarea');
+  const addBtn = document.getElementById('add').addEventListener('click', addTask);
+  const sections = document.querySelectorAll('section');
+
+  function addTask(e) {
+    e.preventDefault();
+    
+    if (task.value && description.value && date.value) {
+      const article = document.createElement('article');
+      const div = create('div', '', 'flex');
+      const startBtn = create('button', 'Start', 'green');
+      const deleteBtn = create('button', 'Delete', 'red');
+      const finishBtn = create('button', 'Finish', 'orange');
+
+      article.appendChild(create('h3', task.value));
+      article.appendChild(create('p', `Description: ${description.value}`));
+      article.appendChild(create('p', `Due Date: ${date.value}`));
+      div.appendChild(startBtn);
+      div.appendChild(deleteBtn);
+      article.appendChild(div);
+
+      startBtn.addEventListener('click', (e) => {
+        sections[2].children[1].appendChild(article);
+
+        finishBtn.addEventListener('click', (e) => {
+          sections[3].children[1].appendChild(article);
+          div.remove();
+        });
+
+        div.appendChild(finishBtn);
+        e.target.remove();
+      });
+
+      deleteBtn.addEventListener('click', () => article.remove());
+      
+      sections[1].children[1].appendChild(article);
+      Array.from(document.querySelectorAll('input, textarea')).forEach(x => x.value = '');
+    }
+  }
+  
+  function create(type, text, className) {
+    const element = document.createElement(type);
+    element.appendChild(document.createTextNode(text));
+    if (className) {
+      element.classList.add(className);
+    }
+    return element;
+  }
 }
