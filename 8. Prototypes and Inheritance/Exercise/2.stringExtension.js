8796aed66b088de68e7bfe9a03dba007e86654c2
flnx <32278279+flnx@users.noreply.github.com>
2022-10-13 12:41:05 +0300

    Upd

--- 8. Prototypes and Inheritance/Exercise/2.stringExtension.js
+++ 8. Prototypes and Inheritance/Exercise/2.stringExtension.js
@@ -32,18 +32,18 @@
   };
 
   String.format = function (str, ...params) {
-    params.forEach((x, i) => str = str.replace(`{${i}}`, x));
+    params.forEach((x, i) => (str = str.replace(`{${i}}`, x)));
     return str;
   };
 })();
 
 let str = 'my string';
-str = str.ensureStart('my'); // 'my string'       // 'my' already present
-str = str.ensureStart('hello '); // 'hello my string'
-str = str.truncate(16); // 'hello my string' // length is 15
-str = str.truncate(14); // 'hello my...'     // length is 11
-str = str.truncate(8); // 'hello...'
-str = str.truncate(4); // '...'
-str = str.truncate(2); // '..'
-str = String.format('The {0} {1} fox', 'quick', 'brown'); // 'The quick brown fox'
-str = String.format('jumps {0} {1}', 'dog'); // 'jumps dog {1}'   // no parameter at 1
+console.log((str = str.ensureStart('my'))); // 'my string'       // 'my' already present
+console.log((str = str.ensureStart('hello '))); // 'hello my string'
+console.log((str = str.truncate(16))); // 'hello my string' // length is 15
+console.log((str = str.truncate(14))); // 'hello my...'     // length is 11
+console.log((str = str.truncate(8))); // 'hello...'
+console.log((str = str.truncate(4))); // '...'
+console.log((str = str.truncate(2))); // '..'
+console.log((str = String.format('The {0} {1} fox', 'quick', 'brown'))); // 'The quick brown fox'
+console.log((str = String.format('jumps {0} {1}', 'dog'))); // 'jumps dog {1}'   // no parameter at 1
