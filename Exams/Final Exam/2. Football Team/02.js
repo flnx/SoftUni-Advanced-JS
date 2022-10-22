551f16a09e2714a3222b45946744e04a4cd3d446
flnx <32278279+flnx@users.noreply.github.com>
2022-10-22 22:19:25 +0300

    updating

--- Exams/Final Exam/2. Football Team/02.js
+++ Exams/Final Exam/2. Football Team/02.js
@@ -26,7 +26,7 @@ class footballTeam {
         output.push(name);
       }
     }
-    return `You successfully invite ${output.join(', ')}`;
+    return `You successfully invite ${output.join(', ')}.`;
   }
 
   signContract(selectedPlayer) {
@@ -70,43 +70,37 @@ class footballTeam {
 
     this.invitedPlayers
       .sort((a, b) => a.name.localeCompare(b.name))
-      .forEach(p => output.push(`Player ${p.name}-${p.playerValue}`));
+      .forEach(x => output.push(`Player ${x.name}-${x.playerValue}`));
 
     return output.join('\n');
   }
 }
 
-// let fTeam = new footballTeam('Barcelona', 'Spain');
-// console.log(
-//   fTeam.newAdditions(['Kylian Mbappé/23/160', 'Lionel Messi/35/50', 'Pau Torres/25/52'])
-// );
+console.log(`---------- 1 ----------`);
+let fTeam = new footballTeam('Barcelona', 'Spain');
+console.log(fTeam.newAdditions(['Kylian Mbappé/23/160', 'Lionel Messi/35/50', 'Pau Torres/25/52']));
+// Output 1
+// You successfully invite Kylian Mbappé, Lionel Messi, Pau Torres.
 
-// // Output 1
-// // You successfully invite Kylian Mbappé, Lionel Messi, Pau Torres.
+console.log(`---------- 2 ----------`);
+let fTeam2 = new footballTeam('Barcelona', 'Spain');
+console.log(fTeam2.newAdditions(['Kylian Mbappé/23/160', 'Lionel Messi/35/50', 'Pau Torres/25/52']));
+console.log(fTeam2.signContract('Lionel Messi/60'));
+console.log(fTeam2.signContract('Kylian Mbappé/240'));
+// console.log(fTeam2.signContract('Barbukov/10')); // throws error
+// Output 2
+// You successfully invite Kylian Mbappé, Lionel Messi, Pau Torres.
+// Congratulations! You sign a contract with Lionel Messi for 60 million dollars.
+// Congratulations! You sign a contract with Kylian Mbappé for 240 million dollars.
+// Uncaught Error: Barbukov is not invited to the selection list!
 
-// // Input 2
-// let fTeam = new footballTeam('Barcelona', 'Spain');
-// console.log(
-//   fTeam.newAdditions(['Kylian Mbappé/23/160', 'Lionel Messi/35/50', 'Pau Torres/25/52'])
-// );
-// console.log(fTeam.signContract('Lionel Messi/60'));
-// console.log(fTeam.signContract('Kylian Mbappé/240'));
-// console.log(fTeam.signContract('Barbukov/10'));
-
-// // Output 2
-// // You successfully invite Kylian Mbappé, Lionel Messi, Pau Torres.
-// // Congratulations! You sign a contract with Lionel Messi for 60 million dollars.
-// // Congratulations! You sign a contract with Kylian Mbappé for 240 million dollars.
-// // Uncaught Error: Barbukov is not invited to the selection list!
-
-// // Input 3
-// let fTeam = new footballTeam("Barcelona", "Spain");
-// console.log(fTeam.newAdditions(["Kylian Mbappé/23/160", "Lionel Messi/35/50", "Pau Torres/25/52"]));
-// console.log(fTeam.ageLimit("Lionel Messi", 33));
-// console.log(fTeam.ageLimit("Kylian Mbappé", 30));
-// console.log(fTeam.ageLimit("Pau Torres", 26));
-// console.log(fTeam.signContract("Kylian Mbappé/240"));
-
+console.log(`---------- 3 ----------`);
+let fTeam3 = new footballTeam("Barcelona", "Spain");
+console.log(fTeam3.newAdditions(["Kylian Mbappé/23/160", "Lionel Messi/35/50", "Pau Torres/25/52"]));
+console.log(fTeam3.ageLimit("Lionel Messi", 33));
+console.log(fTeam3.ageLimit("Kylian Mbappé", 30));
+console.log(fTeam3.ageLimit("Pau Torres", 26));
+console.log(fTeam3.signContract("Kylian Mbappé/240"));
 // Output 3
 // You successfully invite Kylian Mbappé, Lionel Messi, Pau Torres.
 // Lionel Messi is above age limit!
@@ -114,13 +108,12 @@ class footballTeam {
 // Pau Torres will sign a contract for 1 years with Barcelona in Spain!
 // Congratulations! You sign a contract with Kylian Mbappé for 240 million dollars.
 
-// Input 4
-let fTeam = new footballTeam("Barcelona", "Spain");
-console.log(fTeam.newAdditions(["Kylian Mbappé/23/160", "Lionel Messi/35/50", "Pau Torres/25/52"]));
-console.log(fTeam.signContract("Kylian Mbappé/240"));
-console.log(fTeam.ageLimit("Kylian Mbappé", 30));
-console.log(fTeam.transferWindowResult());
-
+console.log(`---------- 4 ----------`);
+let fTeam4 = new footballTeam("Barcelona", "Spain");
+console.log(fTeam4.newAdditions(["Kylian Mbappé/23/160", "Lionel Messi/35/50", "Pau Torres/25/52"]));
+console.log(fTeam4.signContract("Kylian Mbappé/240"));
+console.log(fTeam4.ageLimit("Kylian Mbappé", 30));
+console.log(fTeam4.transferWindowResult());
 // Output 4
 // You successfully invite Kylian Mbappé, Lionel Messi, Pau Torres.
 // Congratulations! You sign a contract with Kylian Mbappé for 240 million dollars.
