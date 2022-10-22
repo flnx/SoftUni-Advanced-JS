c6ac220eb7cbc0083aeaa40bd2a9757a5f693103
flnx <32278279+flnx@users.noreply.github.com>
2022-10-22 17:14:27 +0300

    adding

--- Exams/12. Retake Exam - 10 December 2019/01. Christmas Gifts Delivery/app.js
+++ Exams/12. Retake Exam - 10 December 2019/01. Christmas Gifts Delivery/app.js
@@ -1,3 +1,48 @@
 function solution() {
-    //TO DO
+  const addBtn = document.querySelector('button');
+  const [listSec, sentSec, discSec] = document.querySelectorAll('ul');
+
+  addBtn.addEventListener('click', onAdd);
+
+  function onAdd() {
+    const field = document.querySelector('input[type=text]');
+    const li = document.createElement('li');
+    li.classList.add('gift');
+    li.textContent = field.value;
+
+    const sendBtn = createButton('sendButton', 'Send', onSend);
+    const discardBtn = createButton('discardButton', 'Discard', onDiscard);
+    append(li, sendBtn, discardBtn);
+    listSec.appendChild(li);
+
+    Array.from(listSec.children)
+      .sort((a, b) => a.textContent.localeCompare(b.textContent))
+      .map((x) => listSec.appendChild(x));
+
+    field.value = '';
+
+    function createButton(id, text, func) {
+      const btn = document.createElement('button');
+      btn.id = id;
+      btn.textContent = text;
+      btn.addEventListener('click', func);
+      return btn;
+    }
+
+    function append(parent, ...elements) {
+      elements.map((x) => parent.appendChild(x));
+    }
+
+    function onSend() {
+      sentSec.appendChild(li);
+      sendBtn.remove();
+      discardBtn.remove();
+    }
+
+    function onDiscard() {
+      discSec.appendChild(li);
+      sendBtn.remove();
+      discardBtn.remove();
+    }
+  }
 }
